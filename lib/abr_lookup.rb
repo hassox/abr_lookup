80d12697f2e33baedcd5ee0936052d84c0c7e645
Rob Jacoby <rjacoby@localdirectories.com.au>
2012-06-28 15:15:10 +1000

    updated to v201205

--- lib/abr_lookup.rb
+++ lib/abr_lookup.rb
@@ -8,7 +8,7 @@ module AbrLookup
   autoload :Server, 'abr_lookup/server'
 
   def self.abn_lookup_uri
-    @abn_lookup_url ||= URI.parse("http://abr.business.gov.au/ABRXMLSearchRPC/ABRXMLSearch.asmx/ABRSearchByABN")
+    @abn_lookup_url ||= URI.parse("http://abr.business.gov.au/ABRXMLSearchRPC/ABRXMLSearch.asmx/ABRSearchByABNv201205")
   end
 
   def self.guid
